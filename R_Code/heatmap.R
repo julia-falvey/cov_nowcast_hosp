7f907b053dc01295bba7ebace0eac05e766e6527
jfalvey3 <juliaafalvey@gmail.com>
2025-03-29 18:25:33 -0400

    Clean up heatmap for publication

--- R_Code/heatmap.R
+++ R_Code/heatmap.R
@@ -1,7 +1,8 @@
+# Load Packages -----------------------------------------------------------
 pacman::p_load(plotly, tidyverse, gridExtra, RColorBrewer, xtable)
 
-setwd("~/Desktop/git/cov_nowcast_hosp")
 
+# Functions ---------------------------------------------------------------
 get_baseline_vals <- function(df, algorithm){
   df %>%
     group_by(state, cut_date) %>%
@@ -56,21 +57,24 @@ heatmap_rmse_change <- function(df){
            pct_change = (rmse - baseline_rmse)/baseline_rmse, 
            pct_change_color = if_else(pct_change > 1, 1, pct_change))
 }
-# ARIMA -------------------------------------------------------------------
+
+
+# Pull Model Results ------------------------------------------------------
+# ARIMA
 baseline_arima <- read_rds('results/Univariate/arima_simple_df_list.rds') %>%
   get_baseline_vals(., "ARIMA")
 
 models_arima <- read_rds("results/Multivariate/arimaX_26final.rds") %>%
   get_regressor_vals(., "ARIMA")
 
-# Prophet -----------------------------------------------------------------
+# Prophet
 baseline_prophet <- read_rds('results/Univariate/prophet_simple_df_list.rds') %>%
   get_baseline_vals(., "Prophet")
 
 models_prophet <- read_rds("results/Multivariate/prophet_final.rds") %>%
   get_regressor_vals(., "Prophet")
 
-# BSTS --------------------------------------------------------------------
+# BSTS
 baseline_bsts <- read_rds('results/Univariate/bsts_simple_df_list.rds') %>%
   get_baseline_vals(., "BSTS")
 
@@ -79,8 +83,82 @@ models_bsts <- read_rds("results/Multivariate/bsts_final.rds") %>%
 
 
 
-# Elbow Plot for Baseline -------------------------------------------------
+# Tables ------------------------------------------------------------------
+ccf = read_csv('results/ccf.csv') 
+print(xtable(ccf %>% 
+               group_by(type) %>% 
+               summarise(mean = mean(abs_acf[abs_acf != 0]), 
+                         n_states = sum(abs_acf!=0)) %>%
+               ungroup() %>%
+               arrange(desc(mean)) %>%
+               select(Covariate = type, `Avg. Cross Correlation` = mean, 
+                      `N. States with Data` = n_states), 
+             caption = "Avg. Cross Correlation With Hospitalization Time Series Across States", 
+             label = "tab:ccf_avg"), include.rownames = F)
 
+arima_table <- get_rmse_change(baseline_arima) %>%
+  select(state, cut_date, rmse) %>%
+  group_by(state) %>%
+  mutate(first_val = rmse[cut_date == '2023-04-27'], 
+         val_pct_change = if_else(cut_date == '2023-04-27', rmse, (rmse-first_val)/first_val)) %>%
+  select(state, cut_date, val_pct_change) %>%
+  pivot_wider(names_from = "cut_date", values_from = "val_pct_change")
+
+arima_table %>% ungroup() %>% 
+  summarise(mean(`2024-01-27`))
+
+arima_table %>% filter(`2024-01-27` > 0)
+
+prophet_table <- get_rmse_change(baseline_prophet) %>%
+  select(state, cut_date, rmse) %>%
+  group_by(state) %>%
+  mutate(first_val = rmse[cut_date == '2023-04-27'], 
+         val_pct_change = if_else(cut_date == '2023-04-27', rmse, (rmse-first_val)/first_val)) %>%
+  select(state, cut_date, val_pct_change) %>%
+  pivot_wider(names_from = "cut_date", values_from = "val_pct_change")
+
+prophet_table %>% ungroup() %>% 
+  summarise(mean(`2024-01-27`))
+
+prophet_table %>% filter(`2024-01-27` > 0)
+
+bsts_table <- get_rmse_change(baseline_bsts) %>%
+  select(state, cut_date, rmse) %>%
+  group_by(state) %>%
+  mutate(first_val = rmse[cut_date == '2023-04-27'], 
+         val_pct_change = if_else(cut_date == '2023-04-27', rmse, (rmse-first_val)/first_val)) %>%
+  select(state, cut_date, val_pct_change) %>%
+  pivot_wider(names_from = "cut_date", values_from = "val_pct_change")
+
+bsts_table %>% ungroup() %>% 
+  summarise(mean(`2024-01-27`))
+
+bsts_table %>% filter(`2024-01-27` > 0)
+
+options(xtable.floating = FALSE)
+options(xtable.timestamp = "bsts_table")
+print(xtable(bsts_table), include.rownames=FALSE) 
+
+bind_rows(arima_table %>% mutate(model = "ARIMA"),
+          prophet_table %>% mutate(model = "Prophet")) %>%
+  bind_rows(bsts_table %>% mutate(model = "BSTS")) %>% 
+  group_by(state) %>% filter(`2024-01-27` == min(`2024-01-27`, na.rm = T)) %>% 
+  group_by(model) %>% summarise(n = n())
+
+compare_table <- bind_rows(baseline_arima %>% mutate(model = "ARIMA"),
+                           baseline_prophet %>% mutate(model = "Prophet")) %>%
+  bind_rows(baseline_bsts %>% mutate(model = "BSTS")) %>% 
+  group_by(state, cut_date) %>% 
+  filter(rmse == min(rmse)) %>% 
+  group_by(cut_date, model) %>% summarise(n = n())  %>% 
+  pivot_wider(names_from = cut_date, values_from = n)
+
+options(xtable.floating = FALSE)
+options(xtable.timestamp = "compare_table")
+print(xtable(compare_table), include.rownames=FALSE)
+
+
+# Plots for Univariate Model Estimates ------------------------------------
 p <- bind_rows(get_median_rmse(baseline_arima), 
                get_median_rmse(baseline_prophet)) %>%
   bind_rows(get_median_rmse(baseline_bsts)) %>%
@@ -153,81 +231,8 @@ ggsave("results/RMSE Rel Change from Baseline.pdf",
        dpi = "retina")
 
 
-# Tables ------------------------------------------------------------------
-ccf = read_csv('results/ccf.csv') 
-print(xtable(ccf %>% 
-               group_by(type) %>% 
-               summarise(mean = mean(abs_acf[abs_acf != 0]), 
-                         n_states = sum(abs_acf!=0)) %>%
-               ungroup() %>%
-               arrange(desc(mean)) %>%
-               select(Covariate = type, `Avg. Cross Correlation` = mean, 
-                      `N. States with Data` = n_states), 
-             caption = "Avg. Cross Correlation With Hospitalization Time Series Across States", 
-             label = "tab:ccf_avg"), include.rownames = F)
 
-arima_table <- get_rmse_change(baseline_arima) %>%
-  select(state, cut_date, rmse) %>%
-  group_by(state) %>%
-  mutate(first_val = rmse[cut_date == '2023-04-27'], 
-         val_pct_change = if_else(cut_date == '2023-04-27', rmse, (rmse-first_val)/first_val)) %>%
-  select(state, cut_date, val_pct_change) %>%
-  pivot_wider(names_from = "cut_date", values_from = "val_pct_change")
-
-arima_table %>% ungroup() %>% 
-  summarise(mean(`2024-01-27`))
-
-arima_table %>% filter(`2024-01-27` > 0)
-
-prophet_table <- get_rmse_change(baseline_prophet) %>%
-  select(state, cut_date, rmse) %>%
-  group_by(state) %>%
-  mutate(first_val = rmse[cut_date == '2023-04-27'], 
-         val_pct_change = if_else(cut_date == '2023-04-27', rmse, (rmse-first_val)/first_val)) %>%
-  select(state, cut_date, val_pct_change) %>%
-  pivot_wider(names_from = "cut_date", values_from = "val_pct_change")
-
-prophet_table %>% ungroup() %>% 
-  summarise(mean(`2024-01-27`))
-
-prophet_table %>% filter(`2024-01-27` > 0)
-
-bsts_table <- get_rmse_change(baseline_bsts) %>%
-  select(state, cut_date, rmse) %>%
-  group_by(state) %>%
-  mutate(first_val = rmse[cut_date == '2023-04-27'], 
-         val_pct_change = if_else(cut_date == '2023-04-27', rmse, (rmse-first_val)/first_val)) %>%
-  select(state, cut_date, val_pct_change) %>%
-  pivot_wider(names_from = "cut_date", values_from = "val_pct_change")
-
-bsts_table %>% ungroup() %>% 
-  summarise(mean(`2024-01-27`))
-
-bsts_table %>% filter(`2024-01-27` > 0)
-
-options(xtable.floating = FALSE)
-options(xtable.timestamp = "bsts_table")
-print(xtable(bsts_table), include.rownames=FALSE) 
-
-bind_rows(arima_table %>% mutate(model = "ARIMA"),
-          prophet_table %>% mutate(model = "Prophet")) %>%
-  bind_rows(bsts_table %>% mutate(model = "BSTS")) %>% 
-  group_by(state) %>% filter(`2024-01-27` == min(`2024-01-27`, na.rm = T)) %>% 
-  group_by(model) %>% summarise(n = n())
-
-compare_table <- bind_rows(baseline_arima %>% mutate(model = "ARIMA"),
-          baseline_prophet %>% mutate(model = "Prophet")) %>%
-  bind_rows(baseline_bsts %>% mutate(model = "BSTS")) %>% 
-  group_by(state, cut_date) %>% 
-  filter(rmse == min(rmse)) %>% 
-  group_by(cut_date, model) %>% summarise(n = n())  %>% 
-  pivot_wider(names_from = cut_date, values_from = n)
-
-options(xtable.floating = FALSE)
-options(xtable.timestamp = "compare_table")
-print(xtable(compare_table), include.rownames=FALSE)
-
-# Heatmap -----------------------------------------------------------------
+# Multivariate Plots ------------------------------------------------------
 heatmap_df <- bind_rows(heatmap_rmse_change(bind_rows(baseline_arima, models_arima)), 
                         heatmap_rmse_change(bind_rows(baseline_prophet, models_prophet))) %>%
   bind_rows(heatmap_rmse_change(bind_rows(baseline_bsts, models_bsts))) %>%
@@ -235,8 +240,10 @@ heatmap_df <- bind_rows(heatmap_rmse_change(bind_rows(baseline_arima, models_ari
   #mutate(type = paste(model_type, algorithm_type, sep = ": "))
   mutate(type = factor(model_type, levels = c('All Regressors', 
                                               '% ED Visits', '% ED Visits and Test Positivity', 
-                                              'Wastewater % Detection (Raw)', 'Wastewater % Detection (Raw, Post-Grit, Sludge)')))
+                                              'Wastewater % Detection (Raw)', 
+                                              'Wastewater % Detection (Raw, Post-Grit, Sludge)')))
 
+# Create algorithm-specific heat map
 for(algorithm in unique(heatmap_df$algorithm_type)){
   p <- ggplot(aes(x = state, y = cut_date, fill = pct_change_color), 
               data = heatmap_df %>% filter(algorithm_type == algorithm) %>%
@@ -265,8 +272,7 @@ for(algorithm in unique(heatmap_df$algorithm_type)){
          dpi = "retina") 
 }
 
-
-# Updates -----------------------------------------------------------------
+# Other plots for multivariate models
 p_df <- bind_rows(get_median_rmse(models_arima  %>%
                                  mutate(algorithm_type = model_type)) %>% 
                  mutate(algorithm = "ARIMA"), 
@@ -284,8 +290,6 @@ p <- p_df %>%
   theme_minimal() + 
   facet_wrap(. ~ algorithm_type) +
   labs(y = "Median RMSE", x = "Prediction Window", color = "") 
-
-
 
 p <- p_df %>% 
   bind_rows(bind_rows(get_median_rmse(baseline_arima), 
