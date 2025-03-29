a54e64acf6926dc566672a8126caec443f34d935
jfalvey3 <juliaafalvey@gmail.com>
2025-03-29 18:18:42 -0400

    Clean up prophet_fit for publication

--- R_Code/prophet_fit.R
+++ R_Code/prophet_fit.R
@@ -1,25 +1,9 @@
+# Load Packages -----------------------------------------------------------
 pacman::p_load(aTSA, stats, forecast, prophet, urca, tidyverse, ldsr, scales, xtable)
-setwd("~/Desktop/git/cov_nowcast_hosp")
 
 
-hospitalizations_age <<- read.csv("https://data.cdc.gov/resource/aemt-mg7g.csv?$limit=500000") %>% 
-  dplyr::select(date = week_end_date, state = jurisdiction, weekly_actual_days_reporting_any_data, weekly_percent_days_reporting_any_data, 
-         total_admissions_all_covid_confirmed, total_admissions_adult_covid_confirmed, total_admissions_pediatric_covid_confirmed, 
-         avg_admissions_all_covid_confirmed, percent_adult_covid_admissions,
-         num_hospitals_admissions_all_covid_confirmed) %>%
-  mutate(date = as.Date(date), 
-         mmwr = MMWRweek::MMWRweek(date), 
-         mmwr_date = paste0(mmwr$MMWRyear, '-', if_else(nchar(mmwr$MMWRweek) == 1, 
-                                                        paste0("0", mmwr$MMWRweek), 
-                                                        as.character(mmwr$MMWRweek)))) %>%
-  filter(date <= "2024-05-01") # Filter out optional reporting period 
-
-max_date <- max(hospitalizations_age$date)
-prediction_horizons <- c(max_date %m-% months(3), max_date %m-% months(6), 
-                         max_date %m-% months(9), max_date %m-% months(12)) %>%
-  as.character()
-
-
+# Functions ---------------------------------------------------------------
+# Function for univariate prophet
 fit_prophet <- function(cut_date, data, state) {
   train_data <- data %>% filter(date < cut_date) %>%
     # mutate(cap = max(hosp), floor = 0) %>%
@@ -30,112 +14,31 @@ fit_prophet <- function(cut_date, data, state) {
                    mcmc.samples = 1000)
   n = n_distinct(data %>% filter(date >= cut_date) %>% pull(date))
   future <- make_future_dataframe(model, periods = n, freq = "week") #%>%
-    # mutate(floor = 0, cap = max(train_data$cap))
+  # mutate(floor = 0, cap = max(train_data$cap))
   forecasted <- predict(model, future) %>%
     dplyr::select(date = ds, fitted = yhat)
   
   train_data <- train_data %>% 
     dplyr::select(date = ds, hosp_transformed = y) %>%
     left_join(forecasted)
-
+  
   forecasted <- forecasted %>% 
     inner_join(data %>% filter(date >= cut_date)) %>%
     dplyr::select(date, hosp_transformed, fitted) %>%
     mutate(hosp = exp(hosp_transformed) - 1, 
            `Point Forecast` = exp(fitted) - 1)
   
-  test_metric <- forecasted %>%
-    ungroup() %>% 
-    mutate(mse = (hosp-`Point Forecast`)^2) %>%
-    summarise(rmse = sqrt(sum(mse))/n()) %>%
-    pull(rmse)
-  
   train_data <- train_data %>%
     bind_rows(forecasted) %>%
     mutate(state = state, cut_date = cut_date, 
            hosp = exp(hosp_transformed - 1)) %>%
     dplyr::select(state, cut_date, date, hosp, everything())
-  return(list(data = train_data, rmse = test_metric))
+  return(train_data)
 }
 
-
-out_tibble <- tribble(~state, ~cut_date, ~rmse_val)
-df_list <- tibble()
-for(state_nm in c(state.abb, "DC")){
-  print(state_nm)
-  data <- hospitalizations_age %>%
-    filter(state == state_nm) %>%
-    dplyr::select(date, hosp = total_admissions_all_covid_confirmed) %>%
-    # use box-cox transform on hospital data to avoid negatives in predictions
-    # we assume box-cox order 0 (so log() transform, but add 1 to avoid fitting
-    # log(0) in our model)
-    mutate(hosp_transformed = log(hosp+1))
-  for(predict_cut in prediction_horizons){
-    rmse_spec = fit_prophet(predict_cut, data, state_nm)
-    df_list <- bind_rows(df_list, rmse_spec$data)
-    out_tibble <- bind_rows(out_tibble, 
-                            tibble_row(state = state_nm, 
-                                       cut_date = predict_cut, 
-                                       rmse_val = rmse_spec$rmse))
-  }
-}
-
-
-write_rds(df_list, "prophet_new_horizons_boxcox.rds")
-
-# Do boxcox and inverse boxcox and move MSE into supplement 
-for(predict_cut in prediction_horizons){
-  p <- df_list %>%
-    filter(cut_date == predict_cut, 
-           date >= "2022-07-01") %>%
-    mutate(fitted = exp(fitted) - 1,
-           hosp = exp(hosp_transformed) - 1) %>%
-    rename(`Fitted Period` = fitted, `Forecast Period` = `Point Forecast`) %>%
-    pivot_longer(`Fitted Period`:`Forecast Period`) %>%
-    drop_na() %>%
-    filter(cut_date == as.Date(predict_cut), 
-           date >= "2022-07-01") %>%
-    ggplot(aes(x = date)) + 
-    geom_line(aes(y = hosp, color = "Reported \nHospitalizations")) +
-    geom_point(aes(y = value, color = name), size = 0.5) + 
-    theme_minimal() + 
-    scale_color_manual(values = c("#1B9E77", "#7570B3", "#000000")) + 
-    labs(y = "Hospitalizations", x = "Date", color = "") +
-    facet_wrap(state ~ ., ncol =  4, scales = "free_y")
-  ggsave(paste0("Prophet Results for ", as.Date(predict_cut), ".pdf"), 
-         plot = p, width = 16, height = 10, units = "in", bg = "white", 
-         dpi = "retina")
-}
-
-
-change_table <- out_tibble %>%
-       group_by(state) %>%
-       mutate(rmse_farthest_timepoint = round(rmse_val[row_number()==1], 1), 
-              pct_change = scales::percent((rmse_val - rmse_val[row_number()==1])/rmse_val[row_number()==1], 
-              accuracy = 2)) %>%
-       dplyr::select(-rmse_val) %>%
-       ungroup() %>%
-       pivot_wider(names_from = "cut_date", values_from = "pct_change") %>%
-       dplyr::select(-`2024-01-27`) %>%
-       rename(`2024-01-27` = rmse_farthest_timepoint)
-       
-
-options(xtable.floating = FALSE)
-options(xtable.timestamp = "change_table")
-print(xtable(change_table), include.rownames=FALSE) 
-
-
-
-# With Regressors ---------------------------------------------------------
-
-max_date <- max(hospitalizations_age$date)
-prediction_horizons <- c(max_date %m-% months(3), max_date %m-% months(6), 
-                         max_date %m-% months(9), max_date %m-% months(12)) %>%
-  as.character()
-
-full_data <- read_csv('data/updated_joined_df.csv') # Filter out optional reporting period 
-
-
+# Functions for multivariate prophet - must `add_regressor` for each specific
+# model so combine common attirbutes at start (fit_prophet_reg_start) and 
+# end (fit_prophet_reg_end)
 fit_prophet_reg_start <- function(cut_date, data, state){
   train_data <- data %>% filter(date < cut_date) %>%
     # mutate(cap = max(hosp), floor = 0) %>%
@@ -242,8 +145,76 @@ fit_prophet_regressors <- function(cut_date, data, state) {
   return(fit_prophet_reg_end(model, future, train_data, cut_date, state))
 }
 
+# Pull in Hospitalization Data --------------------------------------------
+hospitalizations_age <<- read.csv("https://data.cdc.gov/resource/aemt-mg7g.csv?$limit=500000") %>% 
+  dplyr::select(date = week_end_date, state = jurisdiction, weekly_actual_days_reporting_any_data, weekly_percent_days_reporting_any_data, 
+         total_admissions_all_covid_confirmed, total_admissions_adult_covid_confirmed, total_admissions_pediatric_covid_confirmed, 
+         avg_admissions_all_covid_confirmed, percent_adult_covid_admissions,
+         num_hospitals_admissions_all_covid_confirmed) %>%
+  mutate(date = as.Date(date), 
+         mmwr = MMWRweek::MMWRweek(date), 
+         mmwr_date = paste0(mmwr$MMWRyear, '-', if_else(nchar(mmwr$MMWRweek) == 1, 
+                                                        paste0("0", mmwr$MMWRweek), 
+                                                        as.character(mmwr$MMWRweek)))) %>%
+  filter(date <= "2024-05-01") # Filter out optional reporting period 
+
+# Set Prediction Horizons -------------------------------------------------
+max_date <- max(hospitalizations_age$date)
+prediction_horizons <- c(max_date %m-% months(3), max_date %m-% months(6), 
+                         max_date %m-% months(9), max_date %m-% months(12)) %>%
+  as.character()
+
+
+# Univariate Prophet Model ------------------------------------------------
 df_list <- tibble()
+for(state_nm in c(state.abb, "DC")){
+  print(state_nm)
+  data <- hospitalizations_age %>%
+    filter(state == state_nm) %>%
+    dplyr::select(date, hosp = total_admissions_all_covid_confirmed) %>%
+    # use box-cox transform on hospital data to avoid negatives in predictions
+    # we assume box-cox order 0 (so log() transform, but add 1 to avoid fitting
+    # log(0) in our model)
+    mutate(hosp_transformed = log(hosp+1))
+  for(predict_cut in prediction_horizons){
+    rmse_spec = fit_prophet(predict_cut, data, state_nm)
+    df_list <- bind_rows(df_list, data)
+  }
+}
+
+# Write results
+write_rds(df_list, "prophet_new_horizons_boxcox.rds")
+
+# Plot
+for(predict_cut in prediction_horizons){
+  p <- df_list %>%
+    filter(cut_date == predict_cut, 
+           date >= "2022-07-01") %>%
+    mutate(fitted = exp(fitted) - 1,
+           hosp = exp(hosp_transformed) - 1) %>%
+    rename(`Fitted Period` = fitted, `Forecast Period` = `Point Forecast`) %>%
+    pivot_longer(`Fitted Period`:`Forecast Period`) %>%
+    drop_na() %>%
+    filter(cut_date == as.Date(predict_cut), 
+           date >= "2022-07-01") %>%
+    ggplot(aes(x = date)) + 
+    geom_line(aes(y = hosp, color = "Reported \nHospitalizations")) +
+    geom_point(aes(y = value, color = name), size = 0.5) + 
+    theme_minimal() + 
+    scale_color_manual(values = c("#1B9E77", "#7570B3", "#000000")) + 
+    labs(y = "Hospitalizations", x = "Date", color = "") +
+    facet_wrap(state ~ ., ncol =  4, scales = "free_y")
+  ggsave(paste0("Prophet Results for ", as.Date(predict_cut), ".pdf"), 
+         plot = p, width = 16, height = 10, units = "in", bg = "white", 
+         dpi = "retina")
+}
+
+# Multivariate Prophet Model ----------------------------------------------
+df_list <- tibble()
+
+full_data <- read_csv('data/updated_joined_df.csv')
 ccf = read_csv('results/ccf.csv') 
+# Run model for each state
 for(state_nm in c(state.abb, "DC")){
   print(state_nm)
   ccf_state = ccf %>%
@@ -294,38 +265,28 @@ for(state_nm in c(state.abb, "DC")){
     filter(           date >= '2022-10-01') %>%
   drop_na()
   for(predict_cut in prediction_horizons){
-    # ed_reg = fit_prophet_ed(predict_cut, data, state_nm) %>%
-    #   mutate(model_type = "% ED Visits")
-    # ed_count_reg = fit_prophet_ed_count(predict_cut, data, state_nm) %>%
-    #   mutate(model_type = "% ED Visits and Test Positivity")
-    # ww_reg = fit_prophet_ww(predict_cut, data, state_nm) %>%
-    #   mutate(model_type = "Wastewater % Detection (Raw)")
+    ed_reg = fit_prophet_ed(predict_cut, data, state_nm) %>%
+      mutate(model_type = "% ED Visits")
+    ed_count_reg = fit_prophet_ed_count(predict_cut, data, state_nm) %>%
+      mutate(model_type = "% ED Visits and Test Positivity")
+    ww_reg = fit_prophet_ww(predict_cut, data, state_nm) %>%
+      mutate(model_type = "Wastewater % Detection (Raw)")
     ww_all_reg = fit_prophet_ww_all(predict_cut, data, state_nm) %>%
       mutate(model_type = "Wastewater % Detection (Raw, Post-Grit, Sludge)")
     all_reg = fit_prophet_regressors(predict_cut, data, state_nm) %>%
       mutate(model_type = "All Regressors")
-    df_list <- df_list %>% #bind_rows(df_list,
-      #                    ed_reg) %>%
-      # bind_rows(ed_count_reg) %>%
-      # bind_rows(ww_reg) %>%
+    df_list <- bind_rows(df_list, ed_reg) %>%
+      bind_rows(ed_count_reg) %>%
+      bind_rows(ww_reg) %>%
       bind_rows(ww_all_reg) %>%
       bind_rows(all_reg) 
   }
 }
 
-
-df_list2 <- read_rds("results/Multivariate/prophet_regressors_no_ww_outlier_lagged.rds") %>%
-  filter(model_type %in% c("% ED Visits", "% ED Visits and Test Positivity", "Wastewater % Detection")) %>%
-  mutate(model_type = if_else(model_type == "Wastewater % Detection", 
-                              "Wastewater % Detection (Raw)", 
-                              model_type))
-
-df_list <- bind_rows(df_list, df_list2) %>%
-  select(state:model_type)
-
+# Write out results
 write_rds(df_list, "results/Multivariate/prophet_final.rds")
 
-# Do boxcox and inverse boxcox and move MSE into supplement 
+# Plot
 for(predict_cut in prediction_horizons){
   for(type in unique(df_list$model_type)){
     p <- df_list %>%
@@ -336,7 +297,6 @@ for(predict_cut in prediction_horizons){
       select(state, cut_date, date, hosp, hosp_transformed, 
              fitted, `Point Forecast`, model_type) %>%
       rename(`Fitted Period` = fitted, `Forecast Period` = `Point Forecast`) %>%
-#      filter(is.na(`Forecast Period`) | `Forecast Period` <= max(hosp) * 100 ) %>%
       pivot_longer(`Fitted Period`:`Forecast Period`) %>%
       drop_na() %>%
       filter(cut_date == as.Date(predict_cut), 
